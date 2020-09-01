f3eb3a01152a67be2636a14f4455bd083c43e824
Elias Delgado <evdelgado99@gmail.com>
2020-09-01 01:41:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,9 +10,13 @@ def join_nested_strings(src)
     element_index = 0
     
     while element_index > src[row_index].count do
+      
       if src[row_index][element_index].class == String
         single_string += src[row_index][]element_index
+      end
+      
       element_index += 1
+    
     end
     
     row_index += 1
